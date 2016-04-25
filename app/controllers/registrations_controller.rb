df153a6baf0064928597256d5216d0bccbd4214b
Pat McAleavey <pmcaleavey8@gmail.com>
2016-04-25 13:04:47 -0400

    route for registration, registrations controller

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,14 @@
+class RegistrationsController < ApplicationController
+  def create
+    @user = User.new(user_name: params[:user_name], first_name: params[:first_name],
+                     last_name: params[:last_name], email: params[:email],
+                     password_digest: params[:password_digest])
+   #@user.ensure_auth_token
+   if @user.save
+     render 'create.json.jbuilder', status: :created
+   else
+     render json: { errors: @user.errors.full_messages },
+            status: :unprocessable_entity
+   end
+  end
+end
