b0713ac07e64b2bc681f5165fc21e4c9d8fc1feb
Pat McAleavey <pmcaleavey8@gmail.com>
2016-04-25 17:35:41 -0400

    registrations controller password_digest to password

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,10 @@
 class RegistrationsController < ApplicationController
-  skip_before_filter :verify_authenticity_token, only: [:create, :login]
+  skip_before_filter :verify_authenticity_token, only: [:create]
 
   def create
     @user = User.new(user_name: params[:user_name], first_name: params[:first_name],
                      last_name: params[:last_name], email: params[:email],
-                     password_digest: params[:password_digest])
+                     password_digest: params[:password])
    @user.ensure_auth_token
    if @user.save
      render 'create.json.jbuilder', status: :created
