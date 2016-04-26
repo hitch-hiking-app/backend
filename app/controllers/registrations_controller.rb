058729f9c08aa3fc99474650afa7953c3adee86a
Pat McAleavey <pmcaleavey8@gmail.com>
2016-04-26 09:59:38 -0400

    both password_digest to password in registration controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,7 +4,7 @@ class RegistrationsController < ApplicationController
   def create
     @user = User.new(user_name: params[:user_name], first_name: params[:first_name],
                      last_name: params[:last_name], email: params[:email],
-                     password_digest: params[:password])
+                     password: params[:password])
    @user.ensure_auth_token
    if @user.save
      render 'create.json.jbuilder', status: :created
