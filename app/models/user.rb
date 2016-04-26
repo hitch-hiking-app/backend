442544f05c3d158d403479028d95fb41fde5a123
Pat McAleavey <pmcaleavey8@gmail.com>
2016-04-26 13:29:31 -0400

    commented out image validation in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
   has_many :hosts
-  has_many :pictures, as: :imageable
-  has_attached_file :image
+  #has_many :pictures, as: :imageable
+  #has_attached_file :image
   has_secure_password
 
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/ 
+  #validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/ 
   validates :email, presence: true, uniqueness: true,
     format: {
       with: /.+\@.+\..+/,
