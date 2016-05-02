520e129be9327281ff8bcca4c453db196681fbb8
Pat McAleavey <pmcaleavey8@gmail.com>
2016-05-02 13:03:33 -0400

    mail working for registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,7 +9,9 @@ class RegistrationsController < ApplicationController
     @picture = @user.pictures.new(image: params[:image])
    @user.ensure_auth_token
    if @user.save
-     UserWelcome.send_signup_email(@user.email).deliver ##UserNotifier
+     #UserWelcome.send_registration_email(@user.email).deliver
+     mail = UserWelcome.welcome(@user)
+     mail.deliver_now
      render 'create.json.jbuilder', status: :created
    else
      render json: { errors: @user.errors.full_messages },
