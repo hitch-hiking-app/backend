9a2b7461035459e12e1c6b746e5f8c1c082eecf7
Pat McAleavey <pmcaleavey8@gmail.com>
2016-05-02 09:54:12 -0400

    development.rb removed redundant code

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -8,24 +8,24 @@ class ProfileController < ApplicationController
 		render "show.json.jbuilder", status: :ok
 	end
 
-  	def update
-    	@user = User.find_by(id: params[:id])
-    	@user.update(user_params)
-		@picture = @user.pictures.first
-      	Picture.update(@picture.id, pic_params)
-      	if @user.save
-        	render "update.json.jbuilder", status: :created
-   		else
-      		render json: { errors: @user.errors.full_messages }, 
-      		status: :unprocessable_entity
-    	end
+	def update
+  	@user = User.find_by(id: params[:id])
+  	@user.update(user_params)
+	  @picture = @user.pictures.first
+    	Picture.update(@picture.id, pic_params)
+    	if @user.save
+      	render "update.json.jbuilder", status: :created
+ 		else
+    		render json: { errors: @user.errors.full_messages },
+    		status: :unprocessable_entity
   	end
+	end
 
-  	def user_params
-		params.permit :user_name, :first_name, :last_name, 
-					  :password, :email, :phone, :car_info,
-					  :home_city, :license_number, :license_plate,
-					  :credit_card_number
+	def user_params
+	params.permit :user_name, :first_name, :last_name,
+				  :password, :email, :phone, :car_info,
+				  :home_city, :license_number, :license_plate,
+				  :credit_card_number
 	end
 
 	def pic_params
