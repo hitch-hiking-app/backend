15a7d635b45699c9dc1d48172e49cf9b3f2d6ce5
Tony Nguyen <tonyviet87@gmail.com>
2016-05-02 16:57:29 -0400

    Changed development domain from local host to heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,11 +54,11 @@ Rails.application.configure do
 
   config.action_mailer.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
-      :port           => '3000',
+      :port           => '587',
       :authentication => :plain,
       :user_name      => ENV['SENDGRID_USERNAME'],
       :password       => ENV['SENDGRID_PASSWORD'],
-      :domain         => 'localhost', ########### WAY TO TEST LOCALLY?
+      :domain         => 'heroku.com', ########### WAY TO TEST LOCALLY?
       :enable_starttls_auto => true
     }
 end
