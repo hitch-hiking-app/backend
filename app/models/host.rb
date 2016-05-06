1de73028f02ede8f9e1967734d2dd16d205dd43e
Pat McAleavey <pmcaleavey8@gmail.com>
2016-05-06 10:32:38 -0400

    syntax error in host model

--- app/models/host.rb
+++ app/models/host.rb
@@ -4,7 +4,7 @@ class Host < ActiveRecord::Base
   has_many :riders, through: :seats, source: :user
 
 
-  validates :seats_available, presence: true
+  validates :seats_available, presence: true,
     format: {
       with: /[1-5][0-9]/,
       message: "You've surpassed the maximum passengers allowed."
