2373a9f183c86e584f5feb2c2b1239005a614e4d
Pat McAleavey <pmcaleavey8@gmail.com>
2016-05-06 10:19:10 -0400

    seats_validation moved to host model

--- app/models/host.rb
+++ app/models/host.rb
@@ -2,4 +2,10 @@ class Host < ActiveRecord::Base
   belongs_to :user
   has_many :seats
   has_many :riders, through: :seats, source: :user
+
+  validates :seats_available, presence: true
+    format: {
+      with: /[1-5][0-9]/,
+      message: "You've surpassed the maximum passengers allowed."
+    }
 end
